084149869fced4432b46b2b86bc633ef89327ea1
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-01-30 23:20:30 +0300

    Add files via upload

---
+++ src/scen/help.py
@@ -0,0 +1,4 @@
+print ("           help")
+print ("    -blue          setting text color as blue")
+print ("    -red           setting text color as red")
+print ("    -green         setting text color as green")

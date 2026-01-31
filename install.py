3f8de261a28e3ee0052a4b202d97031940e8ccab
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-01-31 11:01:35 +0300

    Update install.py

--- install.py
+++ install.py
@@ -4,11 +4,13 @@
 """
 import os
 """maybe the code is unreadable """
-
+# we need to know where is bashrc
 f = os.path.expanduser("~/.bashrc")
 n = "alias wall='python3 /usr/bin/sakura/src\n"
+# now writing alias
 try:
     with open(full_path, 'a', encoding='utf-8') as f:
         f.write(f"\n{n}")
+# if something wrong the except will appear
 except:
     os.system('python src/scen/error.py')

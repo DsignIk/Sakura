7591e5fb9ae0a956a17cf6be7d303e29f8425043
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-01-31 21:16:14 +0300

    Add files via upload

--- install.py
+++ install.py
@@ -3,14 +3,25 @@
     python wallTUI.py <args>
 """
 import os
+import platform
+
 """maybe the code is unreadable """
-# we need to know where is bashrc
-f = os.path.expanduser("~/.bashrc")
-n = "alias wall='python3 /usr/bin/sakura/src\n"
-# now writing alias
-try:
-    with open(full_path, 'a', encoding='utf-8') as f:
-        f.write(f"\n{n}")
-# if something wrong the except will appear
-except:
-    os.system('python src/scen/error.py')
+# if os is the Linux using bashrc
+if platform.system() == 'Linux':
+    f = os.path.expanduser("~/.bashrc")
+    n = "alias sakura='python3 /usr/bin/sakura/src\n"
+    try:
+        with open(f, 'a', encoding='utf-8') as l:
+            l.write(f"\n{n}\n")
+    except:
+        os.system('python src/scen/error.py')
+# if os is the FreeBSD using cshrc
+if platform.system() == 'FreeBSD':
+    f = os.path.expanduser("~/.cshrc")
+    n = "alias sakura 'python3 /usr/bin/sakura/src\n"
+    
+    try:
+        with open(f, 'a', encoding='utf-8') as bsd:
+            bsd.write(f"\n{n}\n")
+    except:
+        os.system(f'python src/scen/error.py')

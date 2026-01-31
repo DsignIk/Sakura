512a4a2fc9e7d5851d037fe7e2ce9a43c2d4b3c7
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-01-31 10:50:39 +0300

    Update help.py

--- src/scen/help.py
+++ src/scen/help.py
@@ -1,3 +1,10 @@
+""" this is the help
+    command
+    I put here the args
+    you can put while running
+"""
+
+
 print ("           help")
 print ("    -blue          setting text color as blue")
 print ("    -red           setting text color as red")

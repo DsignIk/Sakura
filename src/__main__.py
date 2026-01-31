b1be996ccc867b92f515bdc884d432e506c83237
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-01-31 17:18:43 +0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -49,7 +49,7 @@ with open(file_path, "r") as f:
     pamparam = json.load(f)
 if pamparam['browser'] == True:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+        'User-Agent-downloader': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'Referer': 'https://www.google.com'
     }

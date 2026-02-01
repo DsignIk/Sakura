273605a44739234fee1b1ec63e171620783c9ddb
Dsignlk <257780661+DsignIk@users.noreply.github.com>
2026-02-01 11:37:43 +0300

    Add files via upload

---
+++ src/conf/configurator.py
@@ -0,0 +1,35 @@
+""" this installer
+is not ready to use
+when new function will added
+this configuration will be ready
+"""
+
+
+
+
+
+
+import json
+import sys
+import os
+
+data = {
+    "browser": False,
+    "blue": 61,
+    "red": 204,
+    "green": 114
+}
+
+
+print ("               configuration")
+print ("[*] emulate browser?")
+
+# ye this works bad but its works
+# so okay
+
+a = input ('[Y/N] ')
+if a == "Y":
+    print ("[+] custom user agent = user-agent-installer")
+if a == "y":
+    print ("[+] custom user agent = user-agent-installer")
+print ("[-] CONFIGURE NOT READY!")
